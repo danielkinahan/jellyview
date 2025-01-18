74b9d2150c7496bfd90eb55e0a2629c57747ca58
Daniel Kinahan <github@danielkinahan.ca>
2025-01-18 17:03:59 -0500

    simplified app structure

--- app/components/ChartComponent.tsx
+++ app/components/ChartComponent.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
@@ -13,49 +13,37 @@ import {
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-const ChartComponent = () => {
+interface ChartComponentProps {
+  data: any; // Adjust the type according to your data structure
+}
+
+const ChartComponent: React.FC<ChartComponentProps> = ({ data }) => {
   const [chartData, setChartData] = useState<any>(null);
-  const [days, setDays] = useState(28);
-  const [endDate, setEndDate] = useState('2025-01-15');
-  const [timezoneOffset, setTimezoneOffset] = useState(-5);
 
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await fetch(`/api/movies?days=${days}&endDate=${endDate}&timezoneOffset=${timezoneOffset}`);
-        const data = await response.json();
+    if (Array.isArray(data)) {
+      const sortedData = data.sort((a: any, b: any) => b.time - a.time).slice(0, 10);
 
-        if (Array.isArray(data)) {
-          const sortedData = data.sort((a: any, b: any) => b.time - a.time).slice(0, 10);
-
-          setChartData({
-            labels: sortedData.map((item: any) => item.label),
-            datasets: [
-              {
-                label: 'Time',
-                data: sortedData.map((item: any) => item.time),
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1,
-              },
-            ],
-          });
-        } else {
-          console.error('Data is not an array:', data);
-        }
-      } catch (error) {
-        console.error('Failed to fetch data:', error);
-      }
-    };
-
-    getData();
-  }, [days, endDate, timezoneOffset]);
-
-  if (!chartData) return <div>Loading...</div>;
+      setChartData({
+        labels: sortedData.map((item: any) => item.label),
+        datasets: [
+          {
+            label: 'Time',
+            data: sortedData.map((item: any) => item.time),
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+          },
+        ],
+      });
+    } else {
+      console.error('Data is not an array:', data);
+    }
+  }, [data]);
 
   return (
     <div>
-      <Bar data={chartData} />
+      {chartData ? <Bar data={chartData} /> : <p>Loading...</p>}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,54 @@
 import ChartComponent from './components/ChartComponent';
+import { unstable_cache } from 'next/cache';
 
-export default function Home() {
+export default async function Home() {
+  const days = 30;
+  const endDate = new Date().toISOString().slice(0, 10);
+  const stamp = "1737230226092";
+  const timezoneOffset = "-5";
+
+  const params = {
+    days,
+    end_date: endDate,
+    stamp,
+    timezoneOffset,
+  };
+
+  const movieData = await report("user_usage_stats/MoviesReport", params);
+  const tvData = await report("user_usage_stats/GetTvShowsReport", params);
   return (
     <main>
       <h1>Top 10 Movies by Time</h1>
-      <ChartComponent />
+      <ChartComponent data={movieData}/>
+      <h1>Top 10 TV Shows by Time</h1>
+      <ChartComponent data={tvData}/>
     </main>
   );
+}
+
+async function fetchData(endpoint: string, params: any) {
+  const cachedFetch = unstable_cache(fetch, [endpoint], { revalidate: 3600 });
+  const queryString = new URLSearchParams(params).toString();
+  const url = `${process.env.JF_URL}/${endpoint}?${queryString}`
+  console.log(url);
+  const reqHeaders = new Headers();
+  reqHeaders.set("Authorization", `MediaBrowser Token=${process.env.API_KEY}`);
+  const options = {
+    headers: reqHeaders
+  }
+  const response = await cachedFetch(url, options);
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  const data = await response.json();
+  return data;
+}
+
+
+async function report(endpoint: string, params: any) {
+  const cachedFetchData = unstable_cache(fetchData, [endpoint], { revalidate: 3600 });
+  const movieData = await cachedFetchData(endpoint, params);
+  return movieData;
 }
